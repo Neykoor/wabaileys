ecf4494eac6c79bdafe7dfbeb36cb5ca7c62a1d4
Neykoor <cafecafirexos@gmail.com>
2026-06-25 15:32:22 -0700

    Add files via upload

---
+++ lib/WAUSync/Protocols/USyncDeviceProtocol.d.ts
@@ -0,0 +1,22 @@
+export class USyncDeviceProtocol {
+    name: string;
+    getQueryElement(): {
+        tag: string;
+        attrs: {
+            version: string;
+        };
+    };
+    getUserElement(): null;
+    parser(node: any): {
+        deviceList: {
+            id: number;
+            keyIndex: number;
+            isHosted: boolean;
+        }[];
+        keyIndex: {
+            timestamp: number;
+            signedKeyIndex: any;
+            expectedTimestamp: number | undefined;
+        } | undefined;
+    };
+}
